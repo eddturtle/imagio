0fbfc3347caa183da654d304688b9b1562a16928
Edd Turtle <eddturtle@gmail.com>
2015-06-26 08:58:06 +0100

    Convert templates to use a header and footer

--- templates.go
+++ templates.go
@@ -1,18 +1,16 @@
 package main
 
 import (
-	"os"
 	"log"
 	"net/http"
-	"text/template"
+	"html/template"
 )
 
+var templates = template.Must(template.ParseGlob("templates/*"))
+
 func getView(w http.ResponseWriter, name string, object interface{}) {
-	path := "templates/"+name+".html"
-	if _, err := os.Stat(path); err == nil {
-		t := template.Must(template.ParseFiles(path))
-		t.Execute(w, object)
-	} else {
-		log.Fatal("Missing Template " , err)
+	err := templates.ExecuteTemplate(w, name, object)
+	if err != nil {
+		log.Fatal("Cannot Get View ", err)
 	}
 }
