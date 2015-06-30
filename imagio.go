b1b60f692e95bfd8f580f711f6ee1294409a000b
Edd Turtle <eddturtle@gmail.com>
2015-06-30 21:02:13 +0100

    Fix missing dependency

--- imagio.go
+++ imagio.go
@@ -3,11 +3,12 @@ package main
 import (
 	"log"
 	"net/http"
+	"os"
 )
 
 func main() {
 	router := NewRouter()
-	err := http.ListenAndServe(":" + os.Getenv("PORT"), router)
+	err := http.ListenAndServe(":"+os.Getenv("PORT"), router)
 	if err != nil {
 		log.Fatal("ListenAndServe Error: ", err)
 	}
